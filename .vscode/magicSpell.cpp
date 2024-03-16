d0e7d7e3ef89ed3de959b302fe5680ecf9adbb9e
avni <avniiiiiii07@gmail.com>
2024-03-16 08:43:11 +0530

    yes

--- .vscode/magicSpell.cpp
+++ .vscode/magicSpell.cpp
@@ -61,3 +61,15 @@ int main()
     }
     return 0;
 }
+//______________________________________________BREAKING DOWN THE CODE_______________________________________________________________________//
+// Reading Spells: Imagine the wizard is casting spells, and we need to figure out what each spell does. We first ask how many spells the wizard is going to cast (the number of scrolls).
+
+// Identifying Spells: When the wizard casts a spell, we try to figure out what it does. If the wizard shouts out a spell name that we already know (like "Fireball"), we just say, "Ah, it's a Fireball spell!" and we also remember how strong it is (like, if it's very hot or not so hot). Then we write down the spell name and its power.
+
+// Generic Spells: Sometimes the wizard doesn't shout out a spell name we know. Instead, he says something we're not familiar with, like "AquaVitaeAruTaVae". In this case, we try to see if any part of what he said is similar to something we know. If we find a part that's similar to our spell book, we try to find the longest piece that's the same in both the spell he cast and our spell book.
+
+// Figuring it Out: We then shout out the longest piece that's the same as both the wizard's spell and our spell book. For example, if the wizard said "AruTaVae" and our spell book says "AquaVitae", we would say, "Hmm, those two sound similar!" and we'd tell the wizard how many letters they have in common.
+
+// Finishing Up: We keep doing this for each spell the wizard casts until we've figured out all of them.
+
+// So, basically, we listen carefully to what spells the wizard is saying. If it's a spell we know, we remember it. If it's something new, we try to find the most similar thing we already know and tell the wizard how similar they are!
