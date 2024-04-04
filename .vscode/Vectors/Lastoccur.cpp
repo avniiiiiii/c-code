1debafa4c761c3e8f48dcdb0c038f279be947420
avni <avniiiiiii07@gmail.com>
2024-04-04 21:39:38 +0530

    i

--- .vscode/Vectors/Lastoccur.cpp
+++ .vscode/Vectors/Lastoccur.cpp
@@ -5,15 +5,18 @@ using namespace std;
 
 int main()
 {
-    vector<int> v(6); // Resize the vector to size 6
+    vector<int> v(6); // This means the vector initially contains 6 elements
+    // loop reads 6 integers from the standard input (cin) and stores them in the vector v.
     for (int i = 0; i < 6; i++)
     {
         cin >> v[i];
+        // Each integer is accessed using the index i and placed into the corresponding position in the vector.
     }
     cout << "enter x:";
     int x;
     cin >> x;
     int occurrence = -1;
+    // oop iterates over each element of the vector v to find the last occurrence of the value x. If the current element matches x, the index i is stored in the variable occurrence.
     for (int i = 0; i < v.size(); i++)
     {
         if (v[i] == x)
@@ -24,3 +27,4 @@ int main()
     cout << occurrence << endl;
     return 0;
 }
+// the program prints the index of the last occurrence of x in the vector v.
