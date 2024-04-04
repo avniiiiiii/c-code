f44b65182bd8667f8d6488a653bceadf29334f17
avni <avniiiiiii07@gmail.com>
2024-04-04 11:07:23 +0530

    o

--- .vscode/Vectors/operation.cpp
+++ .vscode/Vectors/operation.cpp
@@ -29,6 +29,17 @@ int main()
     v.pop_back();
     cout << "size:" << v.size() << "capacity:"
          << " " << v.capacity() << endl;
-    vector<int> ve = {1, 2, 3, 4};
-    ve.insert(ve.begin() + 1, 9);
+    vector<int> ve;
+    for (int i = 0; i < 5; i++)
+    {
+        int element;
+        cin >> element;
+        v.push_back(element);
+
+        for (int i = 0; i < v.size(); i++)
+        {
+            cout << v[i] << endl;
+        }
+        cout << endl;
+    }
 }
