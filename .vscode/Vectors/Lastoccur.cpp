64e3f797df44a1606bd534c7955dfb5e6818910b
avni <avniiiiiii07@gmail.com>
2024-04-04 21:32:50 +0530

    p

--- .vscode/Vectors/Lastoccur.cpp
+++ .vscode/Vectors/Lastoccur.cpp
@@ -13,7 +13,13 @@ int main()
     cout << "enter x:";
     int x;
     cin >> x;
+    int occurence = -1;
     for (int i = 0; i < v.size(); i++)
     {
+        if (v[i] == x)
+        {
+            occurence = i;
+        }
     }
+    cout << occurence << endl;
 }
