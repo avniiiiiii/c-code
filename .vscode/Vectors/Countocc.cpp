74b68075dc421b2b964fe5426d6acef531307ac9
avni <avniiiiiii07@gmail.com>
2024-04-04 22:02:30 +0530

    p

---
+++ .vscode/Vectors/Countocc.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+
+int main()
+{
+    vector<int> v(6); // This means the vector initially contains 6 elements
+    // loop reads 6 integers from the standard input (cin) and stores them in the vector v.
+    for (int i = 0; i < 6; i++)
+    {
+        cin >> v[i];
+        // Each integer is accessed using the index i and placed into the corresponding position in the vector.
+    }
+    cout << "enter x:";
+    int x;
+    cin >> x;
+}
