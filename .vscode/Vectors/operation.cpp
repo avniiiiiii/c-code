e5089ae40533542ed15a0a09f2de2ab892e54eec
avni <avniiiiiii07@gmail.com>
2024-04-04 10:44:29 +0530

    o

--- .vscode/Vectors/operation.cpp
+++ .vscode/Vectors/operation.cpp
@@ -18,7 +18,11 @@ int main()
     v.push_back(4);
     cout << "size:" << v.size() << "capacity:"
          << " " << v.capacity() << endl;
-    v.resize(5);
+    v.push_back(5);
+    cout << "size:" << v.size() << "capacity:"
+         << " " << v.capacity() << endl;
+
+    v.resize(10);
     cout << "size:" << v.size() << "capacity:"
          << " " << v.capacity() << endl;
     v.pop_back();
