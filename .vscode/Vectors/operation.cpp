b1d43477bba4c7698e253ed127a1535b4ac37845
avni <avniiiiiii07@gmail.com>
2024-04-04 10:30:41 +0530

    i

--- .vscode/Vectors/operation.cpp
+++ .vscode/Vectors/operation.cpp
@@ -1,2 +1,21 @@
 #include <iostream>
+#include <vector>
 using namespace std;
+int main()
+{
+    vector<int> v;
+    cout << "size:" << v.size() << "capacity:"
+         << " " << v.capacity() << endl;
+    v.push_back(1);
+    cout << "size:" << v.size() << "capacity:"
+         << " " << v.capacity() << endl;
+    v.push_back(2);
+    cout << "size:" << v.size() << "capacity:"
+         << " " << v.capacity() << endl;
+    v.push_back(3);
+    cout << "size:" << v.size() << "capacity:"
+         << " " << v.capacity() << endl;
+    v.push_back(4);
+    cout << "size:" << v.size() << "capacity:"
+         << " " << v.capacity() << endl;
+}
