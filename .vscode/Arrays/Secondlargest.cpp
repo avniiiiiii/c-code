1c2b9521225a0bd16fe8d0477901230dfff9f136
avni <avniiiiiii07@gmail.com>
2024-04-05 16:54:54 +0530

    o

--- .vscode/Arrays/Secondlargest.cpp
+++ .vscode/Arrays/Secondlargest.cpp
@@ -1,25 +1,29 @@
 // finf the second largest element in an array
 #include <iostream>
 using namespace std;
-int largestnum(int array[], int size)
+#include <iostream>
+#include <climits> // For INT16_MIN
+using namespace std;
+
+int largestNumIndex(int array[], int size)
 {
     int max = INT16_MIN;
+    int maxIndex = -1;
     for (int i = 0; i < size; i++)
     {
         if (array[i] > max)
         {
             max = array[i];
+            maxIndex = i;
         }
     }
-    return max;
+    return maxIndex;
 }
+
 int main()
 {
-    int array[] = {1,
-                   3,
-                   5,
-                   4,
-                   9,
-                   10};
-    int Largestnum = largestnum(array, 6);
+    int array[] = {1, 3, 5, 4, 9, 10};
+    int largestElementIndex = largestNumIndex(array, 6);
+
+    return 0;
 }
