399462cba228d1efa221771b00c59937fe58cd80
avni <avniiiiiii07@gmail.com>
2024-04-05 11:56:21 +0530

    o

--- .vscode/Vectors/countv.cpp
+++ .vscode/Vectors/countv.cpp
@@ -1,1 +1,28 @@
 // count the no. of elements strictly greater than element x
+#include <iostream>
+#include <vector>
+using namespace std;
+
+int main()
+{
+    vector<int> v(6);
+    for (int i = 0; i < 6; i++)
+    {
+        cin >> v[i];
+    }
+    cout << "enter x:";
+    int x;
+    cin >> x;
+    // cout << "enter y:";
+    // int y;
+    // cin >> y;
+    int count = 0;
+    for (int ele : v)
+    {
+        if (ele > x)
+        {
+            count++;
+        }
+    }
+    cout << count << endl;
+}
