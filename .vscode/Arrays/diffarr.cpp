32f3816b1966fb56589bc0d05bd7c258dbd66528
avni <avniiiiiii07@gmail.com>
2024-04-05 12:21:30 +0530

    p

---
+++ .vscode/Arrays/diffarr.cpp
@@ -0,0 +1,15 @@
+
+
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int arr[] = {1,
+                 2,
+                 1,
+                 2,
+                 1,
+                 2,
+                 1};
+}
