d6f0b461f1182dd690c3211c25e73a21cb263665
avni <avniiiiiii07@gmail.com>
2024-04-05 13:13:04 +0530

    o

--- .vscode/Arrays/Targetsum.cpp
+++ .vscode/Arrays/Targetsum.cpp
@@ -9,11 +9,17 @@ int main()
                    5,
                    1,
                    4};
-
+    int targetsum = 5;
+    int pairs = 0;
     for (int i = 0; i < 5; i++)
     {
         for (int j = i + 1; j < 5; i++)
         {
+            if (array[i] + array[j] == targetsum)
+            {
+                pairs++;
+            }
         }
     }
+    cout << pairs << endl;
 }
