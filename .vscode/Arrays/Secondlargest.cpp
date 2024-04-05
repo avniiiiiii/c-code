2b624d7c69c88d1a95df33c28094eb81a650a7ad
avni <avniiiiiii07@gmail.com>
2024-04-05 16:48:48 +0530

    yespush

--- .vscode/Arrays/Secondlargest.cpp
+++ .vscode/Arrays/Secondlargest.cpp
@@ -3,6 +3,14 @@
 using namespace std;
 int Largestnum(int array[], int size)
 {
+    int max = INT16_MIN;
+    for (int i = 0; i < size; i++)
+    {
+        if (array[i] > max)
+        {
+            max = array[i];
+        }
+    }
 }
 int main()
 {
