a40b830ca94869d59bb3144f6f6783cadbccdd7c
avni <avniiiiiii07@gmail.com>
2024-04-05 13:11:26 +0530

    p

--- .vscode/Arrays/Targetsum.cpp
+++ .vscode/Arrays/Targetsum.cpp
@@ -4,9 +4,16 @@ using namespace std;
 
 int main()
 {
-    int array[] = { 3,
-                    2,
-                    5,
-                    1,
-                    4 }
+    int array[] = {3,
+                   2,
+                   5,
+                   1,
+                   4};
+
+    for (int i = 0; i < 5; i++)
+    {
+        for (int j = i + 1; j < 5; i++)
+        {
+        }
+    }
 }
